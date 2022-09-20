08617badade0a76bf96fad977832483f0f347f6d
LeonardoEzio <33782470+LeonardoEzio@users.noreply.github.com>
2022-09-20 12:21:37 +0800

    kafka消费示例

---
+++ kafka_demo/kafka_consume.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(
+        " ",#消息主题
+        group_id="python_test_01",
+        bootstrap_servers=[''],
+        auto_offset_reset='earliest' # 默认为latest
+    )
+    while True:
+        for msg in consumer:
+            print(msg)
+
